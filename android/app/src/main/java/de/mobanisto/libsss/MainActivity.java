99d74f6043d8b7afce94ee65b9bc2124c32483eb
Sebastian Kürten <sebastian@topobyte.de>
2020-04-15 21:22:43 +0200

    Android: compile native sss code; use in Activity

--- android/app/src/main/java/de/mobanisto/libsss/MainActivity.java
+++ android/app/src/main/java/de/mobanisto/libsss/MainActivity.java
@@ -33,6 +33,22 @@ public class MainActivity extends AppCompatActivity {
 
   }
 
+  public interface LibSSS extends Library {
+
+    LibSSS INSTANCE = Native.load("sss", LibSSS.class);
+
+    // would like to use byte[][] out but only one-dimensional arrays
+    // are supported by JNA
+    void sss_create_shares(byte[] out, byte[] data, int n, int k);
+
+    void sss_combine_shares(byte[] data, byte[] shares, int k);
+  }
+
+  public static final int KEYSHARE_LEN = 33;
+  public static final int MLEN = 64;
+  public static final int CLEN = MLEN + 16;
+  public static final int SHARE_LEN = CLEN + KEYSHARE_LEN;
+
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
@@ -52,17 +68,49 @@ public class MainActivity extends AppCompatActivity {
 
   private List<String> computeSecrets(CharSequence text)
   {
+    int n = 3;
+    int k = 2;
+
     System.out.println("computing secrets…");
+
+    byte[] data = new byte[MLEN];
+    for (int i = 0; i < data.length; i++) {
+      data[i] = (byte) i;
+    }
+
+    byte[] shares = new byte[n * SHARE_LEN];
+    LibSSS.INSTANCE.sss_create_shares(shares, data, n, k);
+
     List<String> secrets = new ArrayList<>();
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < n; i++) {
       secrets.add(String.format("secret %d: %s", i + 1, text));
 
       byte[] buffer = new byte[100];
       OurLibrary.INSTANCE.foo(buffer);
       secrets.add(Native.toString(buffer));
+
+      byte[] share = getShare(shares, i);
+      secrets.add(toString(share));
     }
     System.out.println("done");
     return secrets;
+  }
+
+  private static byte[] getShare(byte[] shares, int i)
+  {
+    byte[] share = new byte[SHARE_LEN];
+    System.arraycopy(shares, i * SHARE_LEN, share, 0, SHARE_LEN);
+    return share;
+  }
+
+  private static String toString(byte[] share)
+  {
+    StringBuilder buffer = new StringBuilder();
+    for (int i = 0; i < share.length; i++) {
+      byte b = share[i];
+      buffer.append(String.format("%x", b));
+    }
+    return buffer.toString();
   }
 
   private List<TextView> textViewsSecrets = new ArrayList<>();
