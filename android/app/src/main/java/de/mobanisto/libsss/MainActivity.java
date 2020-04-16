0c69e9495aa7905f2b96434bfaa57b3859f2c5e4
Sebastian Kürten <sebastian@topobyte.de>
2020-04-16 07:45:33 +0200

    Android: move sss code to new class SSS

--- android/app/src/main/java/de/mobanisto/libsss/MainActivity.java
+++ android/app/src/main/java/de/mobanisto/libsss/MainActivity.java
@@ -7,24 +7,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.sun.jna.Library;
-import com.sun.jna.Native;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-  public interface LibSSS extends Library {
-
-    LibSSS INSTANCE = Native.load("sss", LibSSS.class);
-
-    // would like to use byte[][] out but only one-dimensional arrays
-    // are supported by JNA
-    void sss_create_shares(byte[] out, byte[] data, int n, int k);
-
-    void sss_combine_shares(byte[] data, byte[] shares, int k);
-  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -33,47 +19,17 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     TextView input = findViewById(R.id.input);
-    input.setText("secret text");
+    input.setText("very secret information");
 
     View button = findViewById(R.id.button);
     button.setOnClickListener(e -> {
       CharSequence text = input.getText();
-      List<byte[]> secrets = computeSecrets(text);
+      List<byte[]> secrets = SSS.computeSecrets(text.toString(), 3, 2);
       clearTextViews();
       displaySecrets(secrets);
-      byte[] recovered = recover(Util.pick(secrets, 0, 1));
+      byte[] recovered = SSS.recover(Util.pick(secrets, 0, 1));
       addTextView("recovered text: " + new String(recovered));
     });
-  }
-
-  private byte[] recover(List<byte[]> shares)
-  {
-    byte[] recovered = new byte[Shares.MLEN];
-    byte[] availableShares = Shares.concat(shares);
-    LibSSS.INSTANCE.sss_combine_shares(recovered, availableShares, shares.size());
-    return recovered;
-  }
-
-  private List<byte[]> computeSecrets(CharSequence text)
-  {
-    int n = 3;
-    int k = 2;
-
-    String secret = "very secret information";
-    System.out.println("computing secrets…");
-
-    byte[] data = Shares.secret(secret);
-
-    byte[] shares = new byte[n * Shares.SHARE_LEN];
-    LibSSS.INSTANCE.sss_create_shares(shares, data, n, k);
-
-    List<byte[]> secrets = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-      byte[] share = Shares.getShare(shares, i);
-      secrets.add(share);
-    }
-    System.out.println("done");
-    return secrets;
   }
 
   private List<TextView> textViews = new ArrayList<>();
---
+++ android/app/src/main/java/de/mobanisto/libsss/SSS.java
@@ -0,0 +1,48 @@
+package de.mobanisto.libsss;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SSS {
+
+  public interface LibSSS extends Library {
+
+    LibSSS INSTANCE = Native.load("sss", LibSSS.class);
+
+    // would like to use byte[][] out but only one-dimensional arrays
+    // are supported by JNA
+    void sss_create_shares(byte[] out, byte[] data, int n, int k);
+
+    void sss_combine_shares(byte[] data, byte[] shares, int k);
+  }
+
+  public static List<byte[]> computeSecrets(String secret, int n, int k)
+  {
+    System.out.println("computing secrets…");
+
+    byte[] data = Shares.secret(secret);
+
+    byte[] shares = new byte[n * Shares.SHARE_LEN];
+    LibSSS.INSTANCE.sss_create_shares(shares, data, n, k);
+
+    List<byte[]> secrets = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      byte[] share = Shares.getShare(shares, i);
+      secrets.add(share);
+    }
+    System.out.println("done");
+    return secrets;
+  }
+
+  public static byte[] recover(List<byte[]> shares)
+  {
+    byte[] recovered = new byte[Shares.MLEN];
+    byte[] availableShares = Shares.concat(shares);
+    LibSSS.INSTANCE.sss_combine_shares(recovered, availableShares, shares.size());
+    return recovered;
+  }
+
+}
